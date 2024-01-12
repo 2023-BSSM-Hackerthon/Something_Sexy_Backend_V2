41a37ca19f1e9ede161bc2a5980ad175e74e42a5
마현우 <mhw35689402@gmail.com>
2024-01-12 15:29:54 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/kr/hs/bssm/weet/WeetApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.bssm.weet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/hs/bssm/weet/WeetApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.bssm.weet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
