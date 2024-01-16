e2931ba65914772e2ce9b6ce4af11d8c9eb0bda5
마현우 <mhw35689402@gmail.com>
2024-01-16 14:49:24 +0900

    feat(global): cors 설정

---
+++ src/main/java/kr/hs/bssm/weet/global/config/CorsConfiguration.java
@@ -0,0 +1,19 @@
+package kr.hs.bssm.weet.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
