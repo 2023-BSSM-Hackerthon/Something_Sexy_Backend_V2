b8966118b59b6285dbfa79e7e04fafc5562681ca
마현우 <mhw35689402@gmail.com>
2024-01-23 00:20:21 +0900

    refactor(global): JwtAuthenticationFilter에서 필터 시작할 때 authentication을 null을 대입하도록 변경

--- src/main/java/kr/hs/bssm/weet/global/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/kr/hs/bssm/weet/global/jwt/filter/JwtAuthenticationFilter.java
@@ -21,6 +21,8 @@ public class JwtAuthenticationFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        ContextHolder.setAuthentication(null);
+
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         String jwt = jwtUtil.resolveToken(httpServletRequest);
 
