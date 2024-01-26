d2b27938e126494665fcec8b5ce7e6bd93fed97b
마현우 <mhw35689402@gmail.com>
2024-01-26 14:38:41 +0900

    feat(form): transactional 어노테이션 추가

--- src/main/java/kr/hs/bssm/weet/application/form/FormService.java
+++ src/main/java/kr/hs/bssm/weet/application/form/FormService.java
@@ -6,6 +6,7 @@ import kr.hs.bssm.weet.domain.form.repository.FormRepository;
 import kr.hs.bssm.weet.presentation.form.dto.request.FormRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,11 +17,13 @@ public class FormService {
     private final FormRepository formRepository;
     private final UserService userService;
 
+    @Transactional(readOnly = true)
     public List<Form> findMyForm() {
         Long userId = userService.findCurrentUser().getId();
         return formRepository.findByUserId(userId);
     }
 
+    @Transactional
     public Long create(FormRequestDto dto) {
         Long userId = userService.findCurrentUser().getId();
         return formRepository.save(dto.toEntity(userId)).getId();
