ea101b2d87f4b49f692dcf8dd965c7a69307f995
마현우 <mhw35689402@gmail.com>
2024-01-28 11:53:46 +0900

    feat(result): Transactional 적용

--- src/main/java/kr/hs/bssm/weet/application/result/ResultService.java
+++ src/main/java/kr/hs/bssm/weet/application/result/ResultService.java
@@ -4,6 +4,7 @@ import kr.hs.bssm.weet.domain.result.repository.ResultRepository;
 import kr.hs.bssm.weet.presentation.result.dto.ResultCreateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,6 +12,7 @@ public class ResultService {
 
     private final ResultRepository resultRepository;
 
+    @Transactional
     public Long create(ResultCreateRequestDto dto) {
         return resultRepository.save(dto.toEntity()).getId();
     }
